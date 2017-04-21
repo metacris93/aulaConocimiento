57d54a2edb794c4ba2f3e2ac66c386e538476911
Cristian <metacris93@gmail.com>
2017-04-20 22:33:29 -0500

    se actualizo el database.php con las credenciales de la base de Hogar de Cristo

--- config/constantes.php
+++ config/constantes.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'APP_GESTION_URL'  => 'http://localhost',
-	'APP_GESTION_PORT' => '8000',
-	'APP_AULA_URL'     => 'http://localhost',
+	'APP_GESTION_URL'  => 'http://192.168.90.12',
+	'APP_GESTION_PORT' => '80',
+	'APP_AULA_URL'     => 'http://192.168.90.12',
 	'APP_AULA_PORT'    => '8001'			
 ];
